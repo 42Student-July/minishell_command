0b77288a46a0357426cfd70a6b4fce67f054a2ab
kiri0314 <t.kirihara0314@gmail.com>
2022-02-01 16:41:38 +0900

    UPGRADE: libヘッダーやマクロを追加

--- includes/command.h
+++ includes/command.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:59:10 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/01 11:11:48 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/01 14:42:14 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,17 @@
 #include <unistd.h>
 #include <string.h>
 
+#include "../lib/ft_printf/ft_printf.h"
+#include "../lib/libft/libft.h"
+#include "../lib/gnl/get_next_line.h"
 #include "color.h"
+
+#define CD "cd"
+#define ECHO "echo"
+#define PWD "pwd"
+#define EXIT "exit"
+#define MY_COMMAND_NUM 4
+#define NAME 0
+
+
+bool	is_my_command(char *const *c, int c_num);
