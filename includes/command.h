9ec1de98aa0b2aacb43404eef356f8be7f2bea4f
bayamasa <mapi0213mgt@gmail.com>
2022-02-01 13:00:53 +0900

    MOD: command実行を可能とした

--- includes/command.h
+++ includes/command.h
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:59:10 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/01 10:31:48 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/01 11:11:48 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "color.h"
--- src/main_test.c
+++ src/main_test.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:57:42 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/01 10:47:13 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/02/01 11:13:27 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int	main(int argc, char *const argv[])
 {
 	char **environ;
-	// char *const *av;
+	char *av[10];
 
 	(void)argc;
-	// argv[0] = "echo";
-	// argv[1] = "Hello World!";
-	// argv[2] = NULL;
-
+	(void)argv;
+	av[0] = "/bin/echo";
+	av[1] = "hello";
+	av[2] = NULL;
 	// av = argv;
-	// 第一引数
-	execve("/bin/sh", argv, environ);
-	
+	// 第1引数 PATH, 第2引数 コマンド名＋実行引数
+	if (execve(av[0], av, environ) == -1)
+	{
+		printf("stderror(perror) : %s\n", strerror(errno));
+	}
 	return (0);
 }
