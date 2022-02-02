6107ae03cc96392c1ef3db1ae21a2b650e24baed
kiri0314 <t.kirihara0314@gmail.com>
2022-02-02 09:23:20 +0900

    CLEAN: normの修正

--- includes/command.h
+++ includes/command.h
@@ -6,31 +6,35 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:59:10 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/01 14:42:14 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/02/02 09:22:24 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef COMMAND_H
+# define COMMAND_H
 
-#include "../lib/ft_printf/ft_printf.h"
-#include "../lib/libft/libft.h"
-#include "../lib/gnl/get_next_line.h"
-#include "color.h"
+# include <sys/types.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <pthread.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
-#define CD "cd"
-#define ECHO "echo"
-#define PWD "pwd"
-#define EXIT "exit"
-#define MY_COMMAND_NUM 4
-#define NAME 0
+# include "../lib/ft_printf/ft_printf.h"
+# include "../lib/libft/libft.h"
+# include "../lib/gnl/get_next_line.h"
+# include "color.h"
 
+# define CD "cd"
+# define ECHO "echo"
+# define PWD "pwd"
+# define EXIT "exit"
+# define MY_COMMAND_NUM 4
+# define NAME 0
 
 bool	is_my_command(char *const *c, int c_num);
+
+#endif
