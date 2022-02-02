46a402d2dfe6b61d17526838ac86ac35dc937721
kiri0314 <t.kirihara0314@gmail.com>
2022-02-02 10:35:17 +0900

    FIX: is_my_command関数の引数を修正

--- includes/command.h
+++ includes/command.h
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:59:10 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/02/02 09:22:24 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/02/02 09:48:35 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,6 @@
 # define MY_COMMAND_NUM 4
 # define NAME 0
 
-bool	is_my_command(char *const *c, int c_num);
+bool	is_my_command(const char *c);
 
 #endif
