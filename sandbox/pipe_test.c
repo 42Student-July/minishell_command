f8c2d2e680bc9838db713c78ad4f88bd7b71ad8a
kiri0314 <t.kirihara0314@gmail.com>
2022-02-03 13:49:26 +0900

    DONE: pipe_test

--- sandbox/pipe_test.c
+++ sandbox/pipe_test.c
@@ -3,7 +3,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <../lib/libft/libft.h>
+#include "../lib/libft/libft.h"
 
 static int	get_pipe_cnt(char **command_lst)
 {
@@ -14,7 +14,7 @@ static int	get_pipe_cnt(char **command_lst)
 	pipe_cnt = 0;
 	while (command_lst[i] != NULL)
 	{
-		if (ft_strncmp(command_lst[i], '|', 1) == 0)
+		if (ft_strncmp(command_lst[i], "|", 1) == 0)
 			pipe_cnt++;
 		i++;
 	}
@@ -26,12 +26,11 @@ static void	read_command_lst(char **command_lst, int *pipe_locate)
 	int	i;
 	int	pipe_cnt;
 
-
 	i = 0;
 	pipe_cnt = 0;
 	while (command_lst[i] != NULL)
 	{
-		if (ft_strncmp(command_lst[i], '|', 1) == 0)
+		if (ft_strncmp(command_lst[i], "|", 1) == 0)
 		{
 			pipe_cnt++;
 			pipe_locate[pipe_cnt] = i;
@@ -41,94 +40,158 @@ static void	read_command_lst(char **command_lst, int *pipe_locate)
 	}
 }
 
-void	init_pfd(int **pfd)
+static int	**malloc_pfd(int pipe_cnt)
 {
-	pfd = ;
+	int	i;
+	int	**pfd;
+
+	pfd = (int **)malloc(sizeof(int) * pipe_cnt);
+	if (pfd == NULL)
+	{
+		// malloc error
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < pipe_cnt)
+	{
+		pfd[i] = (int *)malloc(sizeof(int) * 2);
+		if (pfd[i] == NULL)
+		{
+			// malloc error
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (pfd);
+}
+
+void	wait_pipe(int pipe_cnt)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	while (i < pipe_cnt + 1)
+	{
+		wait(&status);
+		i++;
+	}
+}
+
+void	set_pfd(int pipe_cnt, int command_i, int **pfd)
+{
+	if (command_i == 0)
+	{
+		dup2(pfd[command_i][1], 1);
+		close(pfd[command_i][1]);
+		close(pfd[command_i][0]);
+	}
+	else if (command_i == pipe_cnt)
+	{
+		dup2(pfd[command_i - 1][0], 0);
+		close(pfd[command_i - 1][0]);
+		close(pfd[command_i - 1][1]);
+	}
+	else
+	{
+		dup2(pfd[command_i - 1][0], 0);
+		dup2(pfd[command_i][1], 1);
+		close(pfd[command_i - 1][0]);
+		close(pfd[command_i - 1][1]);
+		close(pfd[command_i][0]);
+		close(pfd[command_i][1]);
+	}
+}
+
+void	free_pfd(int **pfd, int pipe_cnt)
+{
+	int i;
+
+	i = 0;
+	while(i < pipe_cnt)
+	{
+		free(pfd[i]);
+		i++;
+	}
+	free(pdf);
+}
+
+void	loop_pipe_process(char **command_lst, int *pipe_locate, int pipe_cnt)
+{
+	int	command_i;
+	int	**pfd;
+	int	pid;
+
+	pfd = malloc_pfd(pipe_cnt);
+	command_i = 0;
+	while (command_i < pipe_cnt + 1)
+	{
+		if (command_i != pipe_cnt)
+			pipe(pfd[command_i]);
+		pid = fork();
+		if (pid == -1)
+		{
+			// エラー
+			printf("fork error\n");
+		}
+		else if (pid == 0)
+		{
+			// 子プロセス
+			set_pfd(pipe_cnt, command_i, pfd);
+			// ここに今までの処理を書く
+			execvp(command_lst[pipe_locate[command_i] + 1], \
+					command_lst + pipe_locate[command_i] + 1);
+			exit(0);
+		}
+		if (command_i != pipe_cnt)
+		{
+			// 親プロセス
+			close(pfd[command_i][0]);
+			close(pfd[command_i][1]);
+		}
+		command_i++;
+	}
+	free_pfd(pfd);
+}
+
+void	no_pipe_process()
+{
+	printf("pipe = 0\n");
+}
+
+void	pipe_process(char **command_lst, int pipe_cnt)
+{
+	int	*pipe_locate;
+
+	pipe_locate = (int *)malloc(sizeof(int) * pipe_cnt + 1);
+	if (pipe_locate == NULL)
+	{
+		// malloc error
+		exit(EXIT_FAILURE);
+	}
+	pipe_locate[0] = -1;
+	read_command_lst(command_lst, pipe_locate);
+	loop_pipe_process(command_lst, pipe_locate , pipe_cnt);
+	wait_pipe(pipe_cnt);
+	free(pipe_locate);
 }
 
 int main()
 {
-	char *command_lst[] = {"ls", NULL};
-	int i;
-	int	*pipe_locate;
+	char *command_lst[] = {"ls", "|", "wc", "-l", NULL};
 	int	pipe_cnt;
-	int	**pfd;
 
 	pipe_cnt = get_pipe_cnt(command_lst);
 	if (pipe_cnt == 0)
 	{
 		// パイプがないケース
-		// ここに今までの処理を書く
+		no_pipe_process();
 	}
 	else
 	{
 		// パイプがあるケース
-		pipe_locate = (int *)malloc(sizeof(int) * pipe_cnt + 1);
-		pipe_locate[0] = -1;
-		read_command_lst(command_lst, pipe_locate);
-		init_pfd(pfd);
-		pfd = (int *)malloc(sizeof(int) * pipe_cnt);
-		while ()
-		{
-			/* code */
-		}
-
-
+		pipe_process(command_lst, pipe_cnt);
 	}
-	// for (i = 0; argv[i] != NULL; i++)
-	// {
-	// 	if (strcmp(argv[i], "|") == 0)
-	// 	{
-	// 		pipe_cnt++;
-	// 		pipe_locate[pipe_cnt] = i;
-	// 		argv[i] = NULL;
-	// 	}
-	// }
-	// int pfd[9][2];
-
-	for (i = 0; i < pipe_cnt + 1 && pipe_cnt != 0; i++)
-	{
-		if (i != pipe_cnt)
-		{
-			pipe(pfd[i]);  //最後のコマンドでなければパイプを作成
-		}
-		if (fork() == 0)
-		{
-			//子プロセス
-			if (i == 0)
-			{
-				//最初のコマンドなので、標準出力をパイプの入り口へつなげる
-				dup2(pfd[i][1], 1);
-				close(pfd[i][0]); close(pfd[i][1]);
-			}
-			else if (i == pipe_cnt)
-			{
-				//最後のコマンドなので、標準入力をパイプの出口へつなげる
-				dup2(pfd[i - 1][0], 0);
-				close(pfd[i - 1][0]); close(pfd[i - 1][1]);
-			}
-			else
-			{
-				//途中のコマンドなので、上記の処理を両方やる
-				dup2(pfd[i - 1][0], 0);
-				dup2(pfd[i][1], 1);
-				close(pfd[i - 1][0]); close(pfd[i - 1][1]);
-				close(pfd[i][0]); close(pfd[i][1]);
-			}
-			execvp(argv[pipe_locate[i] + 1], argv + pipe_locate[i] + 1);
-			exit(0);
-		}
-		else if (i > 0)
-		{
-			//親プロセス
-			//つなげ終わったパイプは閉じる
-			close(pfd[i - 1][0]); close(pfd[i - 1][1]);
-		}
-	}
-	int status;
-
-	for (i = 0; i < pipe_cnt + 1; i++) {
-		wait(&status);
-	}
+	// free
 	return 0;
 }
