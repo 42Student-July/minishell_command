3617f5c8a9580e50ab7428f3f89f991eccef1731
kiri0314 <t.kirihara0314@gmail.com>
2022-02-04 10:39:52 +0900

    UPGRADE: sigint用のsigactionを追加

--- sandbox/signal_test.c
+++ sandbox/signal_test.c
@@ -1,6 +1,25 @@
 #include "../includes/command.h"
 
+void	new_sigint(int sig)
+{
+	printf("new_sigint\n");
+	exit(1);
+}
 
+void	set_sigint()
+{
+	struct sigaction act;
+	act.sa_handler = new_sigint;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	set_signal()
+{
+	set_sigint();  // ctrl + c
+	set_sigquit(); // ctrl + バックスラッシュ
+}
 
 int main(void)
 {
