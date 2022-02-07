3260b039c65c323739d4e70d674351856828b8ea
kiri0314 <t.kirihara0314@gmail.com>
2022-02-07 09:44:27 +0900

    UPGRADE: bashのsigintの挙動を再現

--- sandbox/signal_test.c
+++ sandbox/signal_test.c
@@ -6,19 +6,23 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define MAX_HISTORY_NO 10
+bool	g_is_waiting_for_input;
 
-void	new_sigint(int sig)
+void	new_sigint(int sig, siginfo_t *info, void *ucontext)
 {
-	printf("new_sigint\n");
-	exit(EXIT_SUCCESS);
-	// メインプログラムを呼び出す
+	(void)ucontext;
+	if (!g_is_waiting_for_input)
+		return ;
+	printf("\n");
+	rl_replace_line("", 0); // プロンプトのバッファをクリア
+	rl_on_new_line();       // プロンプトを次の行に移動したいことを伝える？
+	rl_redisplay();         // プロンプトを再描画
 }
 
 void	set_sigint()
 {
 	struct sigaction act;
-	act.sa_handler = new_sigint;
+	act.sa_sigaction = new_sigint;
 	act.sa_flags = 0;
 	sigemptyset(&act.sa_mask);
 	sigaction(SIGINT, &act, NULL);
@@ -42,37 +46,36 @@ void	set_sigquit()
 void	set_signal()
 {
 	set_sigint();  // ctrl + c
-	set_sigquit(); // ctrl + バックスラッシュ
+	// set_sigquit(); // ctrl + バックスラッシュ
 }
 
 void	test_readline()
 {
-	char *prompt = getenv("PS2");
-	char *line = NULL;
+	char *prompt = "minishell $";
+	char *cmd = NULL;
 	int history_i = 0;
 	HIST_ENTRY *rm_history = NULL;
 
 	while (1)
 	{
-		line = readline(prompt);
-		if (line == NULL)
+		g_is_waiting_for_input = true;
+		cmd = readline(prompt);
+		g_is_waiting_for_input = false;
+		if (cmd == NULL)
 			break ;
-		printf("%s\n", line);
-		add_history(line);
-		if (++history_i > MAX_HISTORY_NO)
-		{
-			rm_history = remove_history(0);
-			free(rm_history);
-		}
-		free(line);
+		printf("%s\n", cmd);
+		if (strlen(cmd) > 0) // 空文字列をヒストリーに入れないための対処法
+			add_history(cmd);
+		printf("start\n");
+		sleep(3);
+		printf("end\n");
+		free(cmd);
 	}
 	printf("\n");
 	clear_history();
 }
 
 /*
-	実行前に環境変数PS2にプロンプト用の文字列を設定する
-	例：export PS2='minishell $'
 	コンパイルにはオプションとして-lreadlineを入れる
 	実行中は上矢印キーで過去のコマンドが入力候補として表示される
 	ctrl+d で終了
@@ -81,7 +84,6 @@ void	test_readline()
 int	main(void)
 {
 	set_signal();
-	printf("done set_signal\n");
 	test_readline();
 	return (EXIT_SUCCESS);
 }
